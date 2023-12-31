86c3cb78523642b6d034affc8f697c387c360f06
Adnan <adnan.krdzalic93@gmail.com>
2023-12-31 21:26:13 +0100

    Auth server.

---
+++ auth-server/src/test/java/ba/com/ak/authserver/AuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ba.com.ak.authserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
